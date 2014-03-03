79e53269d6330b35602388de44ad05f654ab6f35
akun <victorsos@163.com>
2014-03-03 21:19:54 +0800

    demo: news list page, add paginator

--- pyhn/news/urls.py
+++ pyhn/news/urls.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python
+
+
 from django.conf.urls import patterns, url
 
 from news import views
 
+
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='news_index'),
+    url(r'^(?P<cur_page_num>\d+)/$', views.index, name='news_list'),
 )
--- pyhn/news/views.py
+++ pyhn/news/views.py
@@ -5,18 +5,25 @@ from urlparse import urlparse
 import hashlib
 import urllib
 
+from django.core.paginator import EmptyPage, InvalidPage, Paginator
 from django.shortcuts import render
 
 from news.models import Post
 
 
-def index(request):
+def index(request, cur_page_num=1):
+    cur_page_num = int(cur_page_num)
+    num_per_page = 30
     posts = Post.objects.all()
-    for post in posts:
+    paged_object = get_paged_object(posts, cur_page_num, num_per_page)
+    for post in paged_object.object_list:
         if post.url:
             post.netloc = urlparse(post.url).netloc
             post.gravatar_url = get_gravatar_url(post.user.email)
-    return render(request, 'news/index.html', {'posts': posts})
+    return render(request, 'news/index.html', {
+        'paged_object': paged_object,
+        'start_index': num_per_page * (cur_page_num - 1),
+    })
 
 
 def get_gravatar_url(email):
@@ -26,3 +33,18 @@ def get_gravatar_url(email):
     gravatar_url = 'http://s.gravatar.com/avatar/' + hashlib.md5(email.lower()).hexdigest() + '?'
     gravatar_url += urllib.urlencode({'d': default, 's': str(size)})
     return gravatar_url
+
+
+def get_paged_object(source_items, cur_page_num, num_per_page):
+    paginator = Paginator(source_items, num_per_page)
+    try:
+        page = int(cur_page_num)
+    except ValueError:
+        page = 1
+
+    try:
+        paged_object = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        paged_object = paginator.page(paginator.num_pages)
+
+    return paged_object
