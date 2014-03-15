00a071379d9160fda717912e51336ede9efc7c0e
akun <victorsos@163.com>
2014-03-15 12:56:04 +0800

    change urls config style

--- pyhn/news/urls.py
+++ pyhn/news/urls.py
@@ -3,10 +3,8 @@
 
 from django.conf.urls import patterns, url
 
-from news import views
-
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='news_index'),
-    url(r'^(?P<cur_page_num>\d+)/$', views.index, name='news_list'),
+    url(r'^$', 'news.views.index', name='news_index'),
+    url(r'^(?P<cur_page_num>\d+)/$', 'news.views.index', name='news_list'),
 )
