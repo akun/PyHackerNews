7327405d39d0f480b03d4ef09c6ac2c0e30aa3bc
akun <victorsos@163.com>
2014-03-20 20:38:03 +0800

    change urls, use namespace

--- pyhn/pyhn/urls.py
+++ pyhn/pyhn/urls.py
@@ -10,6 +10,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'', include('social.apps.django_app.urls', namespace='social')),
     url(r'^$', 'news.views.index', name='index'),
-    url(r'^news/', include('news.urls')),
+    url(r'^news/', include('news.urls', namespace='news')),
     url(r'^admin/', include(admin.site.urls)),
 )
