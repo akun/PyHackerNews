67587d29e4818c3fd49188b8cb18e4bb648abcd4
akun <6awkun@gmail.com>
2014-06-10 00:43:28 +0800

    fixed KeyError if SECRET_KEY is not in env

--- src/pyhn/settings/common.py
+++ src/pyhn/settings/common.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
