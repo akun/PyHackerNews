340cd6656e0f7001edde4992625afda11633915f
akun <6awkun@gmail.com>
2014-06-24 23:04:45 +0800

    add local settings example

--- src/manage.py
+++ src/manage.py
@@ -6,7 +6,7 @@ import sys
 
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyhn.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyhn.settings.local')
 
     from django.core.management import execute_from_command_line
 
---
+++ src/pyhn/settings/local.example.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+##############
+# Production #
+##############
+#from pyhn.settings.prod import *  # pylint: disable=W0401,W0614
+
+
+#ALLOWED_HOSTS = ['.example.com']
+
+
+###############
+# Development #
+###############
+#from pyhn.settings.dev import *  # pylint: disable=W0401,W0614
