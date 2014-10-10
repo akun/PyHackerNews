c78970055d10e53fc0dea9a8ee31c910a0eeb774
akun <6awkun@gmail.com>
2014-10-11 03:42:22 +0800

    change social urls

--- src/pyhn/urls.py
+++ src/pyhn/urls.py
@@ -6,8 +6,10 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns(
     '',
-    url(r'', include('social.apps.django_app.urls', namespace='social')),
     url(r'^$', 'pyhn.apps.news.views.index.index', name='index'),
+    url(
+        r'^social/', include('social.apps.django_app.urls', namespace='social')
+    ),
     url(r'^news/', include('pyhn.apps.news.urls', namespace='news')),
     url(r'^accounts/', include('pyhn.apps.account.urls', namespace='account')),
     url(
